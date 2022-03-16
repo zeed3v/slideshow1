e4253fdcc99237b4404ba3d76a0e240ec6d59adb
zeed3v <98282880+zeed3v@users.noreply.github.com>
2022-03-16 16:36:26 -0300

    Add files via upload

---
+++ js/slideshow.js
@@ -0,0 +1,26 @@
+(function(){
+
+    "use strict";
+
+    const myImages = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
+    let currentImage = 0;
+
+    document.getElementById('next').onclick = nextPhoto;
+    document.getElementById('previous').onclick = prevPhoto;
+
+    function nextPhoto(){
+        currentImage = currentImage+1;
+
+        if( currentImage > myImages.length-1){currentImage = 0; }
+
+        document.getElementById('myimage').src = myImages[currentImage];      
+    }
+    function prevPhoto(){
+        currentImage = currentImage-1;
+
+        if( currentImage < 0){currentImage = myImages.length-1 }
+
+        document.getElementById('myimage').src = myImages[currentImage];
+    }
+})();
+
